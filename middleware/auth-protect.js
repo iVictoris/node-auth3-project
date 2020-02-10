6570bef0279834a821be896149c9dec9baf6d319
Victor Tran <victor.n.tran@outlook.com>
2020-02-10 15:17:20 -0500

    add jwt.verify and called next inside callback.

--- middleware/auth-protect.js
+++ middleware/auth-protect.js
@@ -1,12 +1,23 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-  const token = req.headers.Authorization;
+  const token = req.header("Authorization");
+  const secret = process.env.JWT_SECRET;
 
   if (!token)
-    return res.status(404).json({
+    return res.status(403).json({
       message: "Access Denied."
     });
 
-  next();
+  // verify token
+  jwt.verify(token, secret, (err, decodedToken) => {
+    if (err)
+      return res.status(403).json({
+        message: "Access Denied. Identity invalid."
+      });
+
+    next();
+  });
 };
+
+module.exports = auth;
