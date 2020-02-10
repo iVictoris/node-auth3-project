d53c519caf3deafa253215487243daf7533f881a
Victor Tran <victor.n.tran@outlook.com>
2020-02-10 02:13:14 -0500

    set up app

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const helmet = require("helmet");
+const express = require("express");
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes go here
+// app.use('/route', router)
+
+module.exports = app; // default export
