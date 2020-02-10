9b36312c4e898109428463cb0e863a00fd08d750
Victor Tran <victor.n.tran@outlook.com>
2020-02-10 13:21:02 -0500

    check for username / password for empty data

--- router/auth-router.js
+++ router/auth-router.js
@@ -1,18 +1,15 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
+router.route("/register").post(async (req, res) => {
+  const { username, password } = req.body;
 
-router
-  .route('/register')
-  .post(async (req, res) => {
-    /* POST  */
-  });
+  if (!username || !password)
+    return res.status(400).json({
+      message: "Missing credentials. Please try again."
+    });
+});
 
-router
-  .route('/login')
-  .post(async (req, res) => {
-    /* POST  */
-  });
-
+router.route("/login").post(async (req, res) => {});
 
 module.exports.router = router;
